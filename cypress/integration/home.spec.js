d9943bc3fcd01c577dbddb58b514b832b5ce1da6
Koen Verheyen <koen@inuits.eu>
2017-09-14 12:02:04 +0200

    Setup Cypress.io dashboard (https://dashboard.cypress.io/#/projects/511wso/runs)

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -6,6 +6,6 @@ describe('The Home page', function() {
     it('should display the title', function() {
         cy.get('h1')
             .should('have.length', 1)
-            .should('have.text', 'Publicaions at LibreCat University');
+            .should('have.text', 'Publications at LibreCat University');
     });
 });
