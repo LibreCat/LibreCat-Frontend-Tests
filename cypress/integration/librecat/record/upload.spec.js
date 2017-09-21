032fae59adbbedaf4ef3e0a31d32a6f6305d9105
Koen Verheyen <koen@inuits.eu>
2017-09-21 17:23:19 +0200

    Upload component test

---
+++ cypress/integration/librecat/record/upload.spec.js
@@ -0,0 +1,67 @@
+describe('The file upload function', function() {
+    beforeEach(function() {
+        cy.login();
+    });
+
+    it('should ', function() {
+        cy.visit('/librecat/record/new?type=book');
+
+        cy.server();
+        cy.route({
+            url: '/librecat/upload',
+            method: 'POST',
+            response: {
+                success: 1,
+                tempid: 'abc123',
+                access_level: 'abc234',
+                date_updated: 'abc345',
+                creator: 'abc456',
+                relation: 'abc567',
+            },
+        }).as('upload');
+
+        cy.get('.dz-file-preview')
+            .should('have.length', 0);
+
+        cy.get('#licenses').as('licenses')
+            .should('exist')
+            .should('not.be.visible');
+
+        cy.get('#pub_license_alert').as('license-alert')
+            .should('have.class', 'alert-info')
+            .should('not.have.class', 'alert-danger');
+
+        cy.fixture('logo.png').as('logo')
+            .get('#uploadFiles.dropzone').then(function($dz) {
+                return Cypress.Blob.base64StringToBlob(this.logo, 'image/png')
+                    .then((blob) => {
+                        blob.name = 'logo.png';
+
+                        let dropzone = $dz.prop('dropzone');
+                        dropzone.addFile(blob);
+
+                        cy.log('Added file logo.png');
+                    });
+            });
+
+        cy.wait('@upload');
+
+        cy.get('.dz-file-preview')
+            .should('have.length', 1);
+
+        cy.get('#filename_abc123 strong')
+            .prop('innerText')
+            .should('eq', 'logo.png');
+
+        cy.get('#access_abc123').should('have.text', 'abc234');
+        cy.get('#updated_abc123').should('have.text', 'abc345');
+        cy.get('#creator_abc123').should('have.text', 'abc456');
+        cy.get('#relation_abc123').should('have.text', 'abc567');
+
+        cy.get('@licenses').should('be.visible');
+
+        cy.get('@license-alert')
+            .should('not.have.class', 'alert-info')
+            .should('have.class', 'alert-danger');
+    });
+});
