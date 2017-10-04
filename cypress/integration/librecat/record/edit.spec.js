0f50c6b2392b8056ed502e1286d401df7cb585aa
Koen Verheyen <koen@inuits.eu>
2017-10-04 13:07:17 +0200

    Fixed tests after fontawesome integration

--- cypress/integration/librecat/record/edit.spec.js
+++ cypress/integration/librecat/record/edit.spec.js
@@ -1,19 +1,19 @@
-describe('The Edit publication page', function() {
-    beforeEach(function() {
+describe('The Edit publication page', function () {
+    beforeEach(function () {
         cy.login('einstein', 'einstein');
     });
 
-    it('should load successfully', function() {
+    it('should load successfully', function () {
         cy.visit('/librecat/record/edit/2737388');
     });
 
-    describe('The author field', function() {
-        it('should have 3 authors', function() {
+    describe('The author field', function () {
+        it('should have 3 authors', function () {
             cy.get('#creator .row').should('have.length', 3);
         });
 
-        it('should be able to add an author', function() {
-            cy.get('#last_name_0 ~ .input-group-addon .glyphicon-plus')
+        it('should be able to add an author', function () {
+            cy.get('#last_name_0 ~ .input-group-addon .fa-plus')
                 .click();
 
             cy.get('#creator .row').should('have.length', 4);
@@ -22,7 +22,7 @@ describe('The Edit publication page', function() {
             cy.get('#last_name_3').should('have.value', '');
         });
 
-        it('should be able to change to an internal author', function() {
+        it('should be able to change to an internal author', function () {
             cy.get('#first_name_3').as('first')
                 .should('not.be.readonly')
                 .type('Elvis');
@@ -51,16 +51,16 @@ describe('The Edit publication page', function() {
             cy.get('@last').should('be.readonly');
         });
 
-        it('should be able to remove an author', function() {
+        it('should be able to remove an author', function () {
             cy.get('#creator .row').should('have.length', 4);
 
-            cy.get('#last_name_3 ~ .input-group-addon .glyphicon-minus')
+            cy.get('#last_name_3 ~ .input-group-addon .fa-minus')
                 .click();
 
             cy.get('#creator .row').should('have.length', 3);
         });
 
-        it('should be ordered', function() {
+        it('should be ordered', function () {
             cy.get('#first_name_0').should('have.value', 'Marilyn');
             cy.get('#last_name_0').should('have.value', 'Monroe');
 
@@ -71,9 +71,9 @@ describe('The Edit publication page', function() {
             cy.get('#last_name_2').should('have.value', 'Du Prez');
         });
 
-        it('should be able to reorder authors', function() {
+        it('should be able to reorder authors', function () {
             cy.get('#creator .row:eq(0) span:contains("First Name")')
-                .trigger('mousedown', {which: 1})
+                .trigger('mousedown', { which: 1 })
                 .trigger('mousemove', {pageX: 369, pageY: 500})
                 .trigger('mouseup');
 
