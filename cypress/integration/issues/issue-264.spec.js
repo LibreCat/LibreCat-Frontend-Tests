9e4852ace2347d78a4ccff8f0b6b9da735a2f861
Koen Verheyen <koen.verheyen@inuits.eu>
2017-10-13 12:24:07 +0200

    Test for issue #264

---
+++ cypress/integration/issues/issue-264.spec.js
@@ -0,0 +1,17 @@
+// Test for https://github.com/LibreCat/LibreCat/issues/264
+
+describe('Issue #264: Searching for "Peeters Netherlands"', function() {
+    it('should results in record 2737384', function() {
+        cy.visit('/');
+
+        cy.get('input[name=q]').type('Peeters Netherlands{enter}');
+
+        cy.contains('PUB-ID: 2737384').should('be.visible');
+
+        cy.contains('a', 'Regime change at a distance: Austria and the Southern Netherlands following ' +
+            'the war of the Spanish succession, 1716-1725')
+            .should('be.visible')
+            .should('have.attr', 'href')
+            .should('end.with', '/publication/2737384');
+    });
+});
