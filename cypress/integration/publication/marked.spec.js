c29bf4714cee64e50750bf72da5e157f2504fdba
Koen Verheyen <verheyen.koen@gmail.com>
2019-03-18 14:22:37 +0100

    Fixed marked publications tests (don't work in Chrome)

--- cypress/integration/publication/marked.spec.js
+++ cypress/integration/publication/marked.spec.js
@@ -1,139 +1,126 @@
-describe('The mark/unmark publication feature', function() {
-    it('should be possible to mark and unmark publications', function() {
-        let title1 = '\'Good As Gone\' Doesn\'t Quite Get To Greatness';
-        let title2 = 'A C++ Implementation of the reversed Attentional Vector Sum (rAVS) model';
+describe('The mark/unmark publication feature', function () {
+  it('should be possible to mark and unmark publications', function () {
+    let title1 = "'Good As Gone' Doesn't Quite Get To Greatness"
+    let title2 = 'A C++ Implementation of the reversed Attentional Vector Sum (rAVS) model'
 
-        cy.server();
-        cy.route('/marked_total*').as('marked_total');
+    cy.server()
+    cy.route('/marked_total*').as('marked_total')
 
-        cy.visit('/record?sort=title.asc');
+    cy.visit('/record?sort=title.asc')
 
-        cy.get('.row .citation-block-div a').as('titles')
-            .first()
-            .should('have.text', title1)
-            .click();
-        cy.wait('@marked_total');
+    cy.get('.row .citation-block-div a')
+      .as('titles')
+      .first()
+      .should('have.text', title1)
+      .click()
+    cy.wait('@marked_total')
 
-        cy.get('.label.total-marked').as('marked')
-            .should('have.text', '0');
+    cy.get('.label.total-marked').should('have.text', '0')
 
-        cy.contains('.nav a', ' Mark/Unmark publication').as('mark')
-            .click();
+    cy.contains('.nav a', ' Mark/Unmark publication').click()
 
-        cy.get('@marked')
-            .should('have.text', '1');
+    cy.get('.label.total-marked').should('have.text', '1')
 
-        cy.go('back');
-        cy.wait('@marked_total');
+    cy.go('back')
+    cy.wait('@marked_total')
 
-        cy.get('@titles')
-            .eq(1)
-            .should('have.text', title2)
-            .click();
-        cy.wait('@marked_total');
+    cy.get('@titles')
+      .eq(1)
+      .should('have.text', title2)
+      .click()
+    cy.wait('@marked_total')
 
-        cy.get('@marked').should('have.text', '1');
+    cy.get('.label.total-marked').should('have.text', '1')
 
-        cy.contains('.nav a', ' Mark/Unmark publication').as('mark')
-            .click();
+    cy.contains('.nav a', ' Mark/Unmark publication').click()
 
-        cy.get('@marked')
-            .should('have.text', '2')
-            .click();
-        cy.wait('@marked_total');
+    cy.get('.label.total-marked')
+      .should('have.text', '2')
+      .click()
+    cy.wait('@marked_total')
 
-        cy.get('main .row:nth-child(2) ul.nav li.active a')
-            .should('have.text', '2 Marked Publication(s)');
+    cy.get('main .row:nth-child(2) ul.nav li.active a').should('have.text', '2 Marked Publication(s)')
 
-        cy.get('.markedme')
-            .should('have.length', 2);
+    cy.get('.markedme').should('have.length', 2)
 
-        cy.get('.markedme:nth-of-type(1) .citation-block-div a')
-            .should('have.text', title1);
+    cy.get('.markedme:nth-of-type(1) .citation-block-div a').should('have.text', title1)
 
-        cy.get('.markedme:nth-of-type(2) .citation-block-div a')
-            .should('have.text', title2);
+    cy.get('.markedme:nth-of-type(2) .citation-block-div a').should('have.text', title2)
 
-        cy.get('.markedme')
-            .last()
-            .trigger('mousedown', {which: 1})
-            .trigger('mousemove', {pageX: 579, pageY: 283})
-            .trigger('mouseup');
+    cy.get('.markedme')
+      .last()
+      .trigger('mousedown', { which: 1 })
+      .trigger('mousemove', { pageX: 579, pageY: 283 })
+      .trigger('mouseup')
 
-        cy.reload();
+    cy.reload()
 
-        cy.get('.markedme:nth-of-type(1) .citation-block-div a')
-            .should('have.text', title2);
+    cy.get('.markedme:nth-of-type(1) .citation-block-div a').should('have.text', title2)
 
-        cy.get('.markedme:nth-of-type(2) .citation-block-div a')
-            .should('have.text', title1);
+    cy.get('.markedme:nth-of-type(2) .citation-block-div a').should('have.text', title1)
 
-        cy.contains('#export_facet a', 'BibTeX')
-            .prop('href')
-            .then(cy.request)
-            .then(function(response) {
-                expect(response.headers['content-disposition']).to.match(/^attachment; filename=/);
-                expect(response.headers['content-type']).to.eq('text/x-bibtex; charset=utf-8');
-            });
+    cy.contains('#export_facet a', 'BibTeX')
+      .prop('href')
+      .then(cy.request)
+      .then(function (response) {
+        expect(response.headers['content-disposition']).to.match(/^attachment; filename=/)
+        expect(response.headers['content-type']).to.eq('text/x-bibtex; charset=utf-8')
+      })
 
-        cy.contains('.mark_all', 'Unmark all')
-            .click();
+    cy.contains('.mark_all', 'Unmark all').click()
 
-        cy.get('.mark_all')
-            .should('not.exist');
+    cy.get('.mark_all').should('not.exist')
 
-        cy.get('.markedme')
-            .should('not.exist');
+    cy.get('.markedme').should('not.exist')
 
-        cy.contains('You don\'t have any publications \'marked\' yet.')
-            .should('be.visible');
-    });
+    cy.contains("You don't have any publications 'marked' yet.").should('be.visible')
+  })
 
-    it('should display the correct count when navigating backwards to the list page', function() {
-        cy.server();
-        cy.visit('/record');
+  it('should display the correct count when navigating backwards to the list page', function () {
+    cy.server()
+    cy.visit('/record')
 
-        cy.get('.total-marked').as('total')
-            .should('have.text', '0');
+    cy.get('.total-marked').should('have.text', '0')
 
-        cy.get('.citation-block-div a')
-            .random()
-            .click();
+    cy.get('.citation-block-div a')
+      .random()
+      .click()
 
-        cy.get('a.mark').as('mark').click(); // Mark
+    cy.get('a.mark').click() // Mark
 
-        cy.get('@total').should('have.text', '1');
+    cy.get('.total-marked').should('have.text', '1')
 
-        cy.route('/marked_total*').as('marked_total');
+    cy.route('/marked_total*').as('marked_total')
 
-        cy.go('back');
+    cy.go('back')
 
-        cy.wait('@marked_total');
+    cy.wait('@marked_total')
 
-        cy.get('@total').should('have.text', '1');
-    });
+    cy.get('.total-marked').should('have.text', '1')
+  })
 
-    it('should display the correct count when navigating backwards to the detail page', function() {
-        cy.server();
-        cy.visit('/record/2737390');
+  it('should display the correct count when navigating backwards to the detail page', function () {
+    cy.server()
+    cy.visit('/record/2737390')
 
-        cy.get('.total-marked').as('total')
-            .should('have.text', '0');
+    cy.get('.total-marked').should('have.text', '0')
 
-        cy.get('a.mark').as('mark').click(); // Mark
+    cy.get('a.mark')
+      .as('mark')
+      .click() // Mark
 
-        cy.get('@total')
-            .should('have.text', '1')
-            .click(); // Navigates to /marked route
+    cy.get('.total-marked')
+      .should('have.text', '1')
+      .click() // Navigates to /marked route
 
-        cy.get('@mark').click(); // Unmark
+    cy.get('@mark').click() // Unmark
 
-        cy.route('/marked_total*').as('marked_total');
+    cy.route('/marked_total*').as('marked_total')
 
-        cy.go('back');
+    cy.go('back')
 
-        cy.wait('@marked_total');
+    cy.wait('@marked_total')
 
-        cy.get('@total').should('have.text', '0');
-    });
-});
+    cy.get('.total-marked').should('have.text', '0')
+  })
+})
