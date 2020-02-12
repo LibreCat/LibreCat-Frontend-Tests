f0574ec63da570b8edd60f1bffa4b3aaad4e3794
Koen Verheyen <verheyen.koen@gmail.com>
2020-02-12 17:09:08 +0100

    Fixed manage accounts failing test

--- cypress/integration/librecat/admin/account.spec.js
+++ cypress/integration/librecat/admin/account.spec.js
@@ -1,58 +1,58 @@
 describe('The Manage Accounts page', function() {
-    it('should not be publically exposed', function() {
-        cy.visit('/librecat/admin/account');
+  it('should not be publically exposed', function() {
+    cy.visit('/librecat/admin/account')
 
-        cy.url().should('match', /\/login\?return_url=.*%2Flibrecat%2Fadmin%2Faccount$/);
+    cy.url().should('match', /\/login\?return_url=.*%2Flibrecat%2Fadmin%2Faccount$/)
 
-        cy.get('.page-header h1').should('not.exist');
+    cy.get('.page-header h1').should('not.exist')
 
-        cy.get('#id_login').type('einstein');
-        cy.get('#id_password').type('einstein{enter}');
+    cy.get('#id_login').type('einstein')
+    cy.get('#id_password').type('einstein{enter}')
 
-        cy.url().should('match', /\/librecat\/admin\/account/);
+    cy.url().should('match', /\/librecat\/admin\/account/)
 
-        cy.get('.page-header h1')
-            .should('be.visible')
-            .prop('innerText')
-            .should('eq', 'Manage Account Information');
-    });
+    cy.get('.page-header h1')
+      .should('be.visible')
+      .prop('innerText')
+      .should('eq', 'Manage Account Information')
+  })
 
-    describe('As unauthorized user', function() {
-        it('should not be available', function() {
-            cy.login();
+  describe('As unauthorized user', function() {
+    it('should not be available', function() {
+      cy.login()
 
-            cy.request(
-                {
-                    'url': '/librecat/admin/account',
-                    'failOnStatusCode': false,
-                })
-                .its('status').should('eq', 403);
-        });
-    });
+      cy.request({
+        url: '/librecat/admin/account',
+        failOnStatusCode: false
+      })
+        .its('status')
+        .should('eq', 403)
+    })
+  })
 
-    describe('As authorized user', function() {
-        beforeEach(function() {
-            cy.login('einstein', 'einstein');
-        });
+  describe('As authorized user', function() {
+    beforeEach(function() {
+      cy.login('einstein', 'einstein')
+    })
 
-        it('should display the Add new account button', function() {
-            cy.visit('/librecat/admin/account');
+    it('should display the Add new account button', function() {
+      cy.visit('/librecat/admin/account')
 
-            cy.get('.btn:contains("Add new account")')
-                .should('be.visible');
-        });
+      cy.get('.btn:contains("Add new account")').should('be.visible')
+    })
 
-        it('should display number of results after search', function() {
-            cy.get('h3:contains("Results")').should('not.exist');
+    it('should display number of results after search', function() {
+      cy.contains('h3', '7 Results').should('be.visible')
 
-            cy.get('form#admin-account-search')
-                .within(() => {
-                    cy.get(':submit')
-                        .should('be.visible')
-                        .click();
-                });
+      cy.get('form#admin-account-search').within(() => {
+        cy.get('input[type=text]').type('elvis')
 
-            cy.get('h3:contains("Results")').should('be.visible');
-        });
-    });
-});
+        cy.get(':submit')
+          .should('be.visible')
+          .click()
+      })
+
+      cy.contains('h3', '1 Results').should('be.visible')
+    })
+  })
+})
