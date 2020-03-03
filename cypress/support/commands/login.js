44d15eba96995da28650e08a77d9087e78c67f4a
Koen Verheyen <verheyen.koen@gmail.com>
2020-03-03 16:14:16 +0100

    Fixed login command to use the actual login form

--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -1,34 +1,33 @@
 Cypress.Commands.add('login', (username, password) => {
-    username = username || 'test';
-    password = password || 'secret';
+  username = username || 'test'
+  password = password || 'secret'
 
-    // Initiate command log
-    let consoleProps = {
-        username: username,
-        password: password,
-    };
+  // Initiate command log
+  let consoleProps = {
+    username: username,
+    password: password
+  }
 
-    let log = Cypress.log({
-        name: 'login',
-        message: [username, password],
-        consoleProps: () => {
-            return consoleProps;
-        },
-    });
+  let log = Cypress.log({
+    name: 'login',
+    message: [username, password],
+    consoleProps: () => {
+      return consoleProps
+    }
+  })
 
-    // ... go get it ...
-    cy.request(
-        {
-            method: 'POST',
-            url: '/login',
-            form: true,
-            log: false,
-            body: {
-                user: username,
-                pass: password,
-            },
-        })
-        .then(() => {
-            log.snapshot().end();
-        });
-});
+  const nolog = { log: false }
+
+  // Clear the previous cookie
+  cy.clearCookie('plack_session', nolog)
+
+  // Do the login
+  cy.visit('/login', nolog)
+  cy.get('#id_login', nolog).type(username, nolog)
+  cy.get('#id_password', nolog).type(password, nolog)
+  cy.get(':submit', nolog)
+    .click(nolog)
+    .then(() => {
+      log.snapshot().end()
+    })
+})
