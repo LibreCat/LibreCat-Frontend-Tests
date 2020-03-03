ffb0e326565b05a6f8291bc442b729deef2cf701
Koen Verheyen <verheyen.koen@gmail.com>
2020-03-03 16:31:05 +0100

    Fixed admin tests after login command adaptation

--- cypress/integration/librecat/admin/research_group.spec.js
+++ cypress/integration/librecat/admin/research_group.spec.js
@@ -1,49 +1,48 @@
-describe('The Manage Research Groups page', function() {
-    it('should not be publically exposed', function() {
-        cy.visit('/librecat/admin/research_group');
+describe('The Manage Research Groups page', () => {
+  it('should not be publically exposed', () => {
+    cy.visit('/librecat/admin/research_group')
 
-        cy.url().should('match', /\/login\?return_url=.*%2Flibrecat%2Fadmin%2Fresearch_group$/);
+    cy.url().should('match', /\/login\?return_url=.*%2Flibrecat%2Fadmin%2Fresearch_group$/)
 
-        cy.get('.page-header h1').should('not.exist');
+    cy.get('.page-header h1').should('not.exist')
 
-        cy.get('#id_login').type('einstein');
-        cy.get('#id_password').type('einstein{enter}');
+    cy.get('#id_login').type('einstein')
+    cy.get('#id_password').type('einstein{enter}')
 
-        cy.url().should('match', /\/librecat\/admin\/research_group/);
+    cy.url().should('match', /\/librecat\/admin\/research_group/)
 
-        cy.get('.page-header h1')
-            .should('be.visible')
-            .prop('innerText')
-            .should('eq', 'Manage Research Groups');
-    });
+    cy.get('.page-header h1')
+      .should('be.visible')
+      .prop('innerText')
+      .should('eq', 'Manage Research Groups')
+  })
 
-    describe('As unauthorized user', function() {
-        it('should not be available', function() {
-            cy.login();
+  describe('As unauthorized user', () => {
+    it('should not be available', () => {
+      cy.login()
 
-            cy.request(
-                {
-                    'url': '/librecat/admin/research_group',
-                    'failOnStatusCode': false,
-                })
-                .its('status').should('eq', 403);
-        });
-    });
+      cy.request({
+        url: '/librecat/admin/research_group',
+        failOnStatusCode: false
+      })
+        .its('status')
+        .should('eq', 403)
+    })
+  })
 
-    describe('As authorized user', function() {
-        beforeEach(function() {
-            cy.login('einstein', 'einstein');
-        });
+  describe('As authorized user', () => {
+    beforeEach(() => {
+      cy.login('einstein', 'einstein')
 
-        it('should display the Add new project button', function() {
-            cy.visit('/librecat/admin/research_group');
+      cy.visit('/librecat/admin/research_group')
+    })
 
-            cy.get('.btn:contains("Add new research group")')
-                .should('be.visible');
-        });
+    it('should display the Add new project button', () => {
+      cy.get('.btn:contains("Add new research group")').should('be.visible')
+    })
 
-        it('should display number of results', function() {
-            cy.get('h3:contains("Results")').should('be.visible');
-        });
-    });
-});
+    it('should display number of results', () => {
+      cy.get('h3:contains("Results")').should('be.visible')
+    })
+  })
+})
