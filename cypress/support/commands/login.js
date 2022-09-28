bea44b24102494990982919808f32a619d7f4161
Koen Verheyen <verheyen.koen@gmail.com>
2022-09-28 09:25:51 +0200

    Upgraded Cypress & now using cy.session cacheAcrossSpecs option to cache session data globally and only have to perform each login once

--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -2,23 +2,27 @@ Cypress.Commands.add('login', (username, password) => {
   username = username || 'test'
   password = password || 'secret'
 
-  cy.session(username, () => {
-    let log = Cypress.log({
-      name: 'login',
-      message: [username],
-      consoleProps: { username, password },
-    })
-
-    const nolog = { log: false }
-
-    // Do the login
-    cy.visit('/login', nolog)
-    cy.get('#id_login', nolog).type(username, nolog)
-    cy.get('#id_password', nolog).type(password, nolog)
-    cy.get(':submit', nolog)
-      .click(nolog)
-      .then(() => {
-        log.snapshot().end()
+  cy.session(
+    username,
+    () => {
+      let log = Cypress.log({
+        name: 'login',
+        message: [username],
+        consoleProps: { username, password },
       })
-  })
+
+      const nolog = { log: false }
+
+      // Do the login
+      cy.visit('/login', nolog)
+      cy.get('#id_login', nolog).type(username, nolog)
+      cy.get('#id_password', nolog).type(password, nolog)
+      cy.get(':submit', nolog)
+        .click(nolog)
+        .then(() => {
+          log.snapshot().end()
+        })
+    },
+    { cacheAcrossSpecs: true }
+  )
 })
